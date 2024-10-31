8929218eff59264e0e439ae4396c335721d602d0
delicatte_flower <147403083+Luca970@users.noreply.github.com>
2024-10-31 12:05:20 -0300

    a

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 function criarCartao(pergunta,resposta){
-    let conteudio = document.getElementByid("conteudo")
+    let conteudo = document.getElementByid("conteudo")
+    let cartao = document.createElement()
+     cartao.classList = 'cartao'
+
+     cartao.innerHMTL = '<div class="cartao--pergunta">qual o nome do protagonista de cyberpunk2077</div>
+          <div class="cartao--resposta">o nome do peronagem é "V."</div>'
+     
+     
+     conteudo.appendChild(cartao)
 
 }
+
+
+
 
 criarCartao(1,2)
